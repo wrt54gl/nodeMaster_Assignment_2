88a61acc2cf89de3198f04527497e645f5627da7
Wendel Toews <wendel@dynamo-electric.com>
2018-08-03 14:18:13 -0600

    Adds tokens

--- config.js
+++ config.js
@@ -4,5 +4,10 @@
 var config = {};
 
 config.httpPort=3000;
+config.hashingSecret = 'topsecret';
+
+// This is the time that a token is valid in minutes
+config.tokenTime = 300;
+
 
 module.exports=config;
