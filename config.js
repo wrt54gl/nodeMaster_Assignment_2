d37df1d59d335086a83064d77d6167cba9bc5abe
Wendel Toews <wendel@dynamo-electric.com>
2018-08-04 11:07:55 -0600

    re-factor so handlers are in different files

--- config.js
+++ config.js
@@ -9,5 +9,32 @@ config.hashingSecret = 'topsecret';
 // This is the time that a token is valid in minutes
 config.tokenTime = 300;
 
+config.menuItems = {
+  'Hawaiian':{
+    'description':'Hawaiian with fresh ingredients',
+    'price': 12.95
+  },
+  'Peperonni':{
+    'description':'Premuim Peperonni with our special sauce and lots of cheese',
+    'price': 11.78
+  },
+  'Meat Lovers':{
+    'description':'The name says it all',
+    'price': 13.42
+  },
+  'Greek':{
+    'description':'Loaded with feta and olives',
+    'price': 10.78
+  },
+  'Barbeque Chicken':{
+    'description':'Barbeque Chicken grown without antibiotics. So fresh it sometimes twitches',
+    'price': 11.67
+  },
+  'House':{
+    'description':'House Special -- whats left from yesterday. Supprisingly good',
+    'price': 9.78
+  },
+}
+
 
 module.exports=config;
